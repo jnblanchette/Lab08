3dbc743e14c22266bd8ac79e44b928a84d0aafa6
Jarrod <jnblanchette@uri.edu>
2023-03-26 13:10:31 -0400

    mockLogin

---
+++ mockLogin.py
@@ -0,0 +1,8 @@
+userName = input("Hello! Please enter your facebook username! ")
+
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "jarrod" and userPassword == "iHeartCyber!"):
+    print ("Welcome to facebook!")
+else:
+    print ("Wrong credentials, try again!")
